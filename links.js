5d054fded0b3658694b6f97bc957c832ea473f59
boubaker1key <125211353+boubaker1key@users.noreply.github.com>
2024-03-13 15:13:08 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,6 @@
-var thistime= new Date();
-if(!localStorage.getItem('time')){ localStorage.setItem('time',thistime); }
-var time = thistime - new Date(localStorage.getItem('time'));
-if(document.referrer!=="https://happy-living.online/"&&time < 18000000){
+var thistime = new Date();
+if(!localStorage.getItem("time")){ localStorage.setItem("time",thistime); }
+var time = thistime - new Date(localStorage.getItem("time"));
+if(document.referrer!=="https://happy-living.online/" && time < 18000000){
     location.href="https://happy-living.online/zertx";}
+console.log('yes');
